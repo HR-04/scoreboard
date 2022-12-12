841cf982f44f1ea1b36606c1cd950f0eae66c612
HR-04 <harinirengarajan203@gmail.com>
2022-12-12 15:39:10 +0530

    scoreboard added

---
+++ index.js
@@ -0,0 +1,12 @@
+let homePoint=document.getElementById("homept")
+function increment(inc){
+
+homePoint.innerText=Number(homePoint.innerText)+inc
+
+}
+
+let guestPoint=document.getElementById("guestpt")
+
+function incre(ginc){
+    guestPoint.innerText=Number(guestPoint.innerText)+ginc
+}
